657c4b319878b2dea5489b81450a81a99cdd911d
Brad Kilkenney <bkilkenney@gmail.com>
2016-11-04 07:50:00 -0400

    Initial Commit

---
+++ ToursDesktop/Models/TourStop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToursDesktop.Models {
+	//Added a comment
+	public class TourStop {
+		public int StopNumber { get; set; }
+		public string Name { get; set; }
+		public string Latitude { get; set; }
+		public string Longitude { get; set; }
+		public string Description { get; set; }
+		public string Phone { get; set; }
+		public string ImageUrl { get; set; }
+	}
+}
